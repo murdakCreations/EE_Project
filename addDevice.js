17d1d00c253cca221f722a00c1939c1e2782fa59
murdakCreations <thisisimportant419@gmail.com>
2022-04-24 12:23:26 +0800

    fourth commit

--- addDevice.js
+++ addDevice.js
@@ -2,3 +2,18 @@ function addImg(modalId){
     var modal = document.getElementById(modalId);
     modal.style.display = "block";
 }
+
+let saveBtn = document.getElementById("saveImg");
+saveBtn.addEventListener('click', function(){
+    var fileName = document.getElementById('imgFile').files[0].name;
+    var modal = document.getElementById('modalForm');
+    modal.style.display = "none";
+
+    // create new image
+    const newElement = document.createElement("img");
+    const areaOfAct = document.getElementById('imgArea');
+    newElement.src = fileName;
+    newElement.style.width='100px';
+    newElement.style.height='100px';
+    areaOfAct.appendChild(newElement);
+});
