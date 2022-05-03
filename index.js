c05780d44ab4c425151fc54d6121ba57a8cc6c09
murdakCreations <thisisimportant419@gmail.com>
2022-05-03 22:44:35 +0800

    tenth commit

---
+++ index.js
@@ -0,0 +1,6 @@
+let addImgBtn = document.getElementById("addImg");
+addImgBtn.addEventListener('click', () => {
+    // display modal
+    let modalForm = document.getElementById("modalForm");
+    modalForm.style.display = 'block'; 
+});
