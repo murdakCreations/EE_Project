23eb822a565f736eb48d0ac43da7947892719074
murdakCreations <thisisimportant419@gmail.com>
2022-05-04 11:58:38 +0800

    eleventh commit

--- index.js
+++ index.js
@@ -1,6 +1,37 @@
 let addImgBtn = document.getElementById("addImg");
+let modalForm = document.getElementById("addImgModal");
 addImgBtn.addEventListener('click', () => {
     // display modal
-    let modalForm = document.getElementById("modalForm");
-    modalForm.style.display = 'block'; 
+    modalForm.style.display = 'flex'; 
+});
+
+let saveBtn = document.getElementById("saveImg");
+saveBtn.addEventListener('click', function(){
+    var fileName = document.getElementById('imgFile').files[0];
+    var btnText = document.getElementById('btnText');
+    // save img to local storage
+
+    const reader = new FileReader();
+    reader.addEventListener('load', ()=>{
+        imgSRC = reader.result;
+        // create background image
+        addImgBtn.style.backgroundImage = 'url('+imgSRC+')';
+        addImgBtn.style.width = '400px';
+        addImgBtn.style.height = '400px';
+        addImgBtn.style.backgroundSize = '400px 400px';
+        modalForm.style.display = 'none';
+        btnText.style.display = 'none';
+    });
+    reader.readAsDataURL(fileName);
+});
+
+let closeBtn = document.getElementById('closeModal');
+closeBtn.addEventListener('click', ()=>{
+    modalForm.style.display = 'none';
+});
+
+let addDevBtn = document.getElementById('addDevice');
+let addDevModal = document.getElementById("addDev");
+addDevBtn.addEventListener('click', () => {
+    addDevModal.style.display = 'flex';
 });
